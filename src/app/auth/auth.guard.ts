b0961b0d191f0c4aab9140f171ea216a399748a4
josephttran <42635012+josephttran@users.noreply.github.com>
2019-03-05 04:53:33 -0800

    Guard route

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const url: string = state.url;
+
+    if (localStorage.getItem('userToken')) {
+      console.log('AuthGuard canActivate called');
+      return true;
+    }
+
+    console.log('fail canActivate');
+    this.authService.redirectUrl = url;
+    this.router.navigate(['/login']);
+
+    return false;
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.canActivate(route, state);
+  }
+}
